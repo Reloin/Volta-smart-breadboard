5fba4c310ccd7293e2f4e7c2a93883f71a7f3a64
Reloin <yihengleong@gmail.com>
2019-05-23 20:17:36 +0800

    updated for all version

--- volta/volta.ino
+++ volta/volta.ino
@@ -1,5 +1,8 @@
-const int resistor = 2000;  //the fixed resistor used
+const int resistor = 2000;   //the fixed resistor used
 const int offset = 0; //Eliminate inaccuracy
+
+const char row[] = "abcdefgh";
+const int pin[] = {A0, A1, A2, A3, A4, A5};
 
 const int a = 2; //3 pin to rule them all
 const int b = 3;
@@ -16,30 +19,43 @@ Serial.begin(9600);
 }
 
 void loop() {
-  // loop through all the cd4015 I/O
-  for (int i = 0; i < 8; ++i){
+  // loop through all the cd4015 I/O, or known as every kaki
+  for (int i = 0; i < 8; ++i){//for each breadboard jack
     digitalWrite(a, HIGH && (i & B00000001)); //still loop
     digitalWrite(b, HIGH && (i & B00000010));
     digitalWrite(c, HIGH && (i & B00000100));
+    for (int x = 1; x < 7; ++x){//for each module
+      int data = analogRead(pin[x]);
+      show(x, i);//display which pin is currently reading
+      Serial.println(" "); //easier to read
+      Serial.write(data);
+      Serial.println("/n");
+      }
     
-    Serial.print("pin ");
-    Serial.print(i);
-    Serial.print(" = ");
-    Serial.println(reading(A0)); // refer to the function
-    delay(500);
     }
 
 
 }
 
-int reading(int pin){
-  double data = analogRead(pin);
-  double vout = 5*data/1023;
-  int r =  resistor * vout /(5-vout);
-  if(data > 1020){
-    r = 0;
-    }else{
-      r -= offset;
-      }
-  return r;
+
+void show(int group, int pin){
+  //see if the received data is from left or right
+  //for left hand side
+  if(pin < 4){
+    if(group == 1 || group == 2){Serial.write("1");}
+    if(group == 3 || group == 4){Serial.write("3");}
+    if(group == 5 || group == 6){Serial.write("5");}
+
+    if(group % 2){pin += 4;}// to shift right hand side group to corresponding location
+    }
+    //for right hand side
+   else if(pin > 3){
+    if(group == 1 || group == 2){Serial.write("2");}
+    if(group == 3 || group == 4){Serial.write("4");}
+    if(group == 5 || group == 6){Serial.write("6");}
+
+    if(!(group % 2)){pin -= 4;}//shift left hand side to corresponding location
+    }
+    
+    Serial.write(row[pin]);
   }
