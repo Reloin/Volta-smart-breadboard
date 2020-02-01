93a8b8135df7183ff4c99bc0c8ddc373e129bdba
Reloin <yihengleong@gmail.com>
2020-02-01 11:05:57 +0800

    move gui element to separate file

--- gui.py
+++ gui.py
@@ -1,34 +1,46 @@
-from PyQt5 import QtWidgets
+# Python program that show components connected to breadboar
+
+from PyQt5 import QtWidgets, QtSvg
 import ctypes
 import sys
+#import readData
+
 
 # window properties
 window_width = 720
-window_heigth = 480
+window_heigth = 550
+btn_width = 200
+btn_height = 50
 title = 'Smart Breadboard'
 
+class ui_window(object):
+    def __init__(self):
+        # make window centered
+        user32 = ctypes.windll.user32
+        xPos = (user32.GetSystemMetrics(0) - window_width)/2
+        yPos = (user32.GetSystemMetrics(1) - window_heigth)/2
+        # make ui
+        self.app = QtWidgets.QApplication(sys.argv)
+        self.win = QtWidgets.QMainWindow()
+        self.win.setGeometry(xPos, yPos, window_width, window_heigth)
+        self.win.setWindowTitle(title)
 
+        # display breadboard
+        self.bbsvg = QtSvg.QSvgWidget('svg/breadboard.svg', self.win)
+        self.bbsvg.setGeometry(20, 20, 1300, 400)
+        self.bbsvg.show()
+        
+        # set the Generate button
+        self.gnrBtn = QtWidgets.QPushButton(self.win)
+        self.gnrBtn.setText('Generate')
+        xBtn = (window_width - btn_width ) / 2
+        self.gnrBtn.setGeometry(xBtn, 475, btn_width, btn_height)
+        self.gnrBtn.clicked.connect(self.clicked)
 
-#function to make interface to show breadboard
-
-def clicked():
-    print('Generate button clicked')
-
-def gui():
-    # make window centered
-    user32 = ctypes.windll.user32
-    xPos = (user32.GetSystemMetrics(0) - window_width)/2
-    yPos = (user32.GetSystemMetrics(1) - window_heigth)/2
-    app = QtWidgets.QApplication(sys.argv)
-    win = QtWidgets.QMainWindow()
-    win.setGeometry(xPos, yPos, window_width, window_heigth)
-    win.setWindowTitle(title)
-
-    # set the Generate button
-    gnrBtn = QtWidgets.QPushButton(win)
-    gnrBtn.setText('Generate')
-    gnrBtn.clicked.connect()
-
-    # make wiindow shows up
-    win.show()
-    sys.exit(app.exec_())
+        # make wiindow shows up
+        self.win.show()
+        sys.exit(self.app.exec_())
+    
+    def clicked(self):
+        #readData.read()
+        print('reading')
--- main.py
+++ main.py
@@ -1,54 +1,7 @@
-# Python program that show components connected to breadboar
+"""
+Code created by Reloin, for breadboard project
+"""
 
-from PyQt5 import QtWidgets, QtSvg
-import drawDiagram
-import ctypes
-import sys
-import serial
+import gui
 
-
-port = 'com3'
-baudRate = 9600
-arduino = serial.Serial(port, baudrate = baudRate, timeout = 1)
-
-# window properties
-window_width = 720
-window_heigth = 480
-btn_width = 200
-btn_height = 50
-title = 'Smart Breadboard'
-
-
-
-def clicked():
-    data = arduino.readline().decode('ascii')
-    print(data)
-
-#function to make interface to show breadboard
-def gui():
-    # make window centered
-    user32 = ctypes.windll.user32
-    xPos = (user32.GetSystemMetrics(0) - window_width)/2
-    yPos = (user32.GetSystemMetrics(1) - window_heigth)/2
-    # make ui
-    app = QtWidgets.QApplication(sys.argv)
-    win = QtWidgets.QMainWindow()
-    win.setGeometry(xPos, yPos, window_width, window_heigth)
-    win.setWindowTitle(title)
-
-    # display breadboard
-    bbsvg = QtSvg.QSvgWidget('svg/breadboard.svg', win)
-    bbsvg.setGeometry(150, -150, 880, 600)
-    bbsvg.show()
-    # set the Generate button
-    gnrBtn = QtWidgets.QPushButton(win)
-    gnrBtn.setText('Generate')
-    xBtn = (window_width - btn_width ) / 2
-    gnrBtn.setGeometry(xBtn, 350, btn_width, btn_height)
-    gnrBtn.clicked.connect(clicked)
-
-    # make wiindow shows up
-    win.show()
-    sys.exit(app.exec_())
-    
-gui()
+win = gui.ui_window()
